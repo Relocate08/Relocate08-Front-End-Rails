7c5f81ada409bdf245b9504064e017b8a60ddb7d
Hope Gochnour <hope.gochnour@gmail.com>
2021-01-06 20:46:06 -0700

    Add welcome page and user model specs
    Co-Authored-By: Brett  <BJSherman80@users.noreply.github.com>
    Co-Authored-By: John Kim <abcdefghijohn@users.noreply.github.com>
    Co-Authored-By: Zach Stearns <zach.stearns92@gmail.com>

---
+++ spec/features/welcome/welcome_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "As an authenticated user" do
+  describe "when I visit /" do
+    it "I should see" do
+      app_description = "Moving to a new place is as exciting as it is stressful. Relocate was developed to help alleviate the stress so you can focus on settling in. What makes a home isn’t just the roof you sleep under, but the neighborhood around you. The average person moves approximately 11.7 times in their lifetime, and that means new gyms, new utilities, and new health care providers, among other things. Relocate's user-friendly interface is a quick way to find and compare all the local businesses and necessities around you based on the location you provide."
+      visit "/"
+
+      expect(page).to have_content("Welcome to Relocate")
+      expect(page).to have_link("Login with Google")
+      expect(page).to have_content(app_description)
+      expect(page).to have_link("Privacy Policy")
+    end
+
+    xit "starts the oauth process when you click on the 'Login with Google' link" do
+      visit "/"
+      click_link("Login with Google")
+      expect(current_path).to eq("/auth/google_oath2")
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:uid) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:token) }
+  end
+end

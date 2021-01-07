34d898cd85a7ab9f5a06f54135ba31e409bc0e37
John Kim <abcdefghijohnk@gmail.com>
2021-01-06 18:31:01 -0700

    Update routes with dashboard route and update sessionscontroller create action
    Co-Authored-By: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-Authored-By: Zach Stearns <zach.stearns92@gmail.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by(uid: request_hash[uid])
+    user = User.find_or_create_by(uid: request_hash[:uid])
     user.uid = request_hash[:uid]
     user.token = request_hash[:credentials][:token]
     user.name = request_hash[:info][:name]
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/auth/google_oauth2', as: :google_login
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/dashboard', to: 'dashboard#index', as: :dashboard
 end

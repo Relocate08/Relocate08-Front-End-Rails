16a70c3f5ad73d9f2c0fc637bc5935877ab702ec
John Kim <abcdefghijohnk@gmail.com>
2021-01-06 18:15:49 -0700

    Create user table
    Co-Authored-By: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-Authored-By: Zach Stearns <zach.stearns92@gmail.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

---
+++ db/migrate/20210107011248_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :uid
+      t.string :email
+      t.string :name
+      t.string :token
+    end
+  end
+end

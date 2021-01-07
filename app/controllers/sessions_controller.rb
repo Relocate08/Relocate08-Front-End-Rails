ca38f487759c4a5e1b5031a9990dc27752bc9e50
John Kim <abcdefghijohnk@gmail.com>
2021-01-06 19:14:49 -0700

    Create destroy action and flash message in sessions controller
    Co-Authored-By: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-Authored-By: Zach Stearns <zach.stearns92@gmail.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,17 @@ class SessionsController < ApplicationController
     user.uid = request_hash[:uid]
     user.token = request_hash[:credentials][:token]
     user.name = request_hash[:info][:name]
+    user.email = request_hash[:info][:email]
     user.save
     session[:user_id] = user.id
+    flash[:success] = "You are now logged in!"
     redirect_to dashboard_path
   end
 
   def destroy
-    
+    session.delete(:user_id)
+    flash[:success] = 'You have been logged out!'
+    redirect_to root_path
   end
 
   private

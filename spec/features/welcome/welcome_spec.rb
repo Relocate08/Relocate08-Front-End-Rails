b506719b22b196163c72ebfb35bd768f7b4579ce
Zach Stearns <zach.stearns92@gmail.com>
2021-01-07 21:24:47 -0700

    Oauth login test almost done, WIP
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -6,7 +6,7 @@ describe 'As an authenticated user' do
   describe 'when I visit /' do
     it 'I should see' do
       app_description = "Moving to a new place is as exciting as it is stressful. Relocate was developed to help alleviate the stress so you can focus on settling in. What makes a home isn’t just the roof you sleep under, but the neighborhood around you. The average person moves approximately 11.7 times in their lifetime, and that means new gyms, new utilities, and new health care providers, among other things. Relocate's user-friendly interface is a quick way to find and compare all the local businesses and necessities around you based on the location you provide."
-      visit '/'
+      visit root_path
 
       expect(page).to have_content('Welcome to Relocate')
       expect(page).to have_link('Login with Google')
@@ -14,10 +14,41 @@ describe 'As an authenticated user' do
       expect(page).to have_link('Privacy Policy')
     end
 
-    xit "starts the oauth process when you click on the 'Login with Google' link" do
-      visit '/'
-      click_link('Login with Google')
-      expect(current_path).to eq('/auth/google_oath2')
+    it "Google OAuth logs in new user" do
+      user_count = User.count
+      expect(user_count).to eq(0)
+
+      stub_omniauth
+      user_count = User.count
+      expect(user_count).to eq(0)
+
+      visit root_path
+
+      click_link 'Login with Google'
+
+      user_count = User.count
+      expect(user_count).to eq(1)
+      user = User.first
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_link('Log Out')
+    end
+
+    it 'Returning Google user is logged in' do
+      stub_omniauth
+      user = create(:omniauth_mock_user)
+      user_count = User.count
+      expect(user_count).to eq(1)
+  
+      visit root_path
+  
+      click_link 'Login with Google'
+  
+      user_count = User.count
+      expect(user_count).to eq(1)
+  
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_link 'Log Out'
     end
   end
 end

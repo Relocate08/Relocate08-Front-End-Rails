b6adb54221f04ea35110550c4c963f5d8ae0a449
Zach Stearns <zach.stearns92@gmail.com>
2021-01-07 21:26:04 -0700

    User factory made
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/factories/user_omniauth.rb
+++ spec/factories/user_omniauth.rb
@@ -2,16 +2,16 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    username { "#{Faker::Games::WorldOfWarcraft.hero} #{Faker::Number.number(digits: 3)}" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
     uid { Faker::Number.within(range: 100000..999999) }
-    google_token { Faker::Number.within(range: 100000..999999) }
-    google_refresh_token { Faker::Number.within(range: 100000..999999) }
+    token { Faker::Number.within(range: 100000..999999) }
   end
 
   factory :omniauth_mock_user, parent: :user do
-    username { 'john@example.com' }
+    name { 'Jimmy Neutron' }
+    email { 'john@example.com' }
     uid {'100000000000000000000'}
-    google_token {'MOCK_OMNIAUTH_GOOGLE_TOKEN'}
-    google_refresh_token {'MOCK_OMNIAUTH_GOOGLE_REFRESH TOKEN'}
+    token {'MOCK_OMNIAUTH_GOOGLE_TOKEN'}
   end
 end

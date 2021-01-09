bd9e8cbb12b95625bc44c25a6fb88d22e36976bf
Kevin Cuadros <kev040798@gmail.com>
2021-01-09 17:42:56 -0500

    Fix travis errors

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    uid {Faker::Alphanumeric.alphanumeric(number: 12)}
-    email {Faker::Internet.email}
-    name {Faker::Name.name}
-    token {Faker::Alphanumeric.alphanumeric(number: 12)}
-  end
-end

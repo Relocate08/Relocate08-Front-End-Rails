c0e2d3b051538fcbcac511b4f5d6ebc520159b42
Kevin Cuadros <kev040798@gmail.com>
2021-01-08 19:38:46 -0500

    Update index test for dashboard and services.
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

---
+++ spec/features/dashboard/dashboard_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'when I visit the dashboard page' do
+    it 'should see a form to fill in a location of search' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      within '.location-search' do
+        fill_in :location, with: '80211'
+        click_on 'Search Location'
+      end
+
+      expect(current_path).to eq('/address')
+
+      within '.buttons' do
+        expect(page).to have_button('Utilities')
+        click_on 'Utilities'
+      end
+
+      expect(current_path).to eq('/80211/utilities')
+      expect(page).to have_content('Utilities')
+      expect(page).to have_link('Electricity')
+    end
+  end
+end
--- spec/features/services/index_spec.rb
+++ spec/features/services/index_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 describe 'As a User' do
   describe 'When I have clicked a service category' do
     it 'should see all the businesses for that category' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       json_response = File.read('spec/fixtures/electric_search.json')
-      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/80211/utilities/electricity').to_return(status: 200, body: json_response, headers: {})
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/yelp?location=80211&service&type=electricity").
+         to_return(status: 200, body: json_response, headers: {})
+
+
       visit '/80211/utilities'
 
       click_on 'Electricity'

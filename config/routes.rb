1ee905a4762194844c2ffd424fed3bd0dc58f4ca
Kevin Cuadros <kev040798@gmail.com>
2021-01-09 16:29:05 -0500

    Update routes to be dynamic
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get '/address', to: 'service_category#index'
   get '/:location/:services', to: 'services#index', as: :service_index
   get '/:location/:services/:type', to: 'service_types#index'
+  get '/:location/:services/:type/:id', to: 'service_types#show'
 end

1c0db85677049ea9daf8d510e0819bcdedf813e6
Kevin Cuadros <kev040798@gmail.com>
2021-01-09 16:35:46 -0500

    Fix poro rubocop

--- app/poros/business.rb
+++ app/poros/business.rb
@@ -1,12 +1,13 @@
 class Business
-	attr_reader :id, :name, :image, :url, :location, :phone, :distance
-	def initialize(data)
-		@id = data[:id]
-		@name = data[:name]
-		@image = data[:image]
-		@url = data[:url]
-		@location = data[:location]
-		@phone = data[:phone]
-		@distance = data[:distance]
-	end
+  attr_reader :id, :name, :image, :url, :location, :phone, :distance
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:name]
+    @image = data[:image]
+    @url = data[:url]
+    @location = data[:location]
+    @phone = data[:phone]
+    @distance = data[:distance]
+  end
 end

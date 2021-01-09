a8d5d64f527d45a7e50e8d0adca54e006b98b87d
Kevin Cuadros <kev040798@gmail.com>
2021-01-09 16:32:01 -0500

    Take away comments.
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -5,16 +5,5 @@ class ServiceTypesController < ApplicationController
 
   def show
     @business = SearchFacade.a_business(params[:location], params[:services], params[:type], params[:id])
-    # conn = Faraday.new(url: 'https://relocate-back-end-rails.herokuapp.com')
-
-    # response = conn.get("/api/v1/yelp") do |req|
-    #   req.params[:location] = params[:location]
-    #   req.params[:service] = params[:service]
-    #   req.params[:type] = params[:type]
-    #   req.params[:id] = params[:id]
-    # end
-
-    # json = JSON.parse(response.body, symbolize_names: true)
-    # @business = Business.new(json)
   end
 end

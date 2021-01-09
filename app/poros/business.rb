3809895f63b01f2fcbac5796b26361a408b5493a
Jake Heft <jakeheft@gmail.com>
2021-01-09 10:41:29 -0700

    Add id to business poro

--- app/poros/business.rb
+++ app/poros/business.rb
@@ -1,6 +1,7 @@
 class Business
-	attr_reader :name, :image, :url, :location, :phone, :distance
+	attr_reader :id, :name, :image, :url, :location, :phone, :distance
 	def initialize(data)
+		@id = data[:id]
 		@name = data[:name]
 		@image = data[:image]
 		@url = data[:url]

5b87e1ebd3aa47e628bd1bf118c20f871d6df71a
Kevin Cuadros <kev040798@gmail.com>
2021-01-09 16:27:50 -0500

    Update the webmocks for index and show.
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>

--- spec/features/services/show_spec.rb
+++ spec/features/services/show_spec.rb
@@ -1,32 +1,25 @@
 require 'rails_helper'
 
 describe 'As a logged in user' do
-	describe 'When I click on a company from the service type index page' do
-		it 'I am taken to that companys show page to see their info' do
-			user = create(:user)
+  describe 'When I click on a company from the service type index page' do
+    it 'I am taken to that companys show page to see their info' do
+      user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      json_response = File.read('spec/fixtures/electric_search.json')
-      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/yelp?location=80211&service&type=Electricity").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'Faraday v1.3.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
+      json_response = File.read('spec/fixtures/show_business_page.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/yelp?id=9s8dfs7fsdmsdf&location=80211&service=utilities&type=Electricity')
+        .to_return(status: 200, body: json_response, headers: {})
 
-			visit '/80211/utilities/Electricity'
+      visit '/80211/utilities/Electricity/9s8dfs7fsdmsdf'
 
-			click_link 'Xcel Energy'
-
-			expect(current_path).to eq('/80211/utilities/Electricity/9s8dfs7fsdmsdf')
-			expect(page).to have_content('Xcel Energy')
-      expect(page).to have_xpath("//img[contains(@src, 'https://s3-media1.fl.yelpcdn.com/bphoto/pDZ8YQ098hOq1RzwbFFyWA/o.jpg')]")
-      expect(page).to have_link('See Xcel Energy on Yelp')
-      expect(page).to have_content('1099 18th St, Ste 3000, Denver, CO, 80202')
-      expect(page).to have_content('(800) 895-4999')
-      expect(page).to have_content(1.02)
-		end
-	end
+      within '.biz-info' do
+        expect(page).to have_content('Xcel Energy')
+        expect(page).to have_xpath("//img[contains(@src, 'https://s3-media1.fl.yelpcdn.com/bphoto/pDZ8YQ098hOq1RzwbFFyWA/o.jpg')]")
+        expect(page).to have_link('See Xcel Energy on Yelp')
+        expect(page).to have_content('1099 18th St, Ste 3000, Denver, CO, 80202')
+        expect(page).to have_content('(800) 895-4999')
+        expect(page).to have_content(1.02)
+      end
+    end
+  end
 end

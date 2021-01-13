7d594cebb4b0948af671aca969c50c491e38f248
Kevin Cuadros <kev040798@gmail.com>
2021-01-13 18:20:25 -0500

    Added the sad path in the index.
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Zach Stearns <64452036+Stearnzy@users.noreply.github.com>

--- app/controllers/service_category_controller.rb
+++ app/controllers/service_category_controller.rb
@@ -1,4 +1,11 @@
 class ServiceCategoryController < ApplicationController
   def index
+    number = params[:location].to_i
+    if number >= 10000 && number.to_s.length <= 5
+      @location = number
+    else
+      flash[:error] = 'Please enter a valid zipcode'
+      redirect_to dashboard_path
+    end
   end
 end

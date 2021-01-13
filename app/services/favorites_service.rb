dac03f99f974c4af09c62a67e280d3124a0438ec
Zach Stearns <zach.stearns92@gmail.com>
2021-01-13 15:15:22 -0700

    Add connections for favorites save, delete, and index.

---
+++ app/services/favorites_service.rb
@@ -0,0 +1,20 @@
+class FavoritesService
+  def self.save_favorite(user_id, yelp_business_id, business_name)
+    response = conn.post("/api/v1/favorites/#{user_id}/#{yelp_business_id}") do |req|
+      req.params[:business_name] = business_name
+    end
+  end
+
+  def self.favorites_list(user_id)
+    response = conn.get("/api/v1/favorites/#{user_id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.delete_favorite(user_id, yelp_business_id)
+    response = conn.delete("/api/v1/favorites/#{user_id}/#{yelp_business_id}")
+  end
+
+  def self.conn
+    Faraday.new(url: 'https://relocate-back-end-rails.herokuapp.com')
+  end
+end

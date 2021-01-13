2171126550881604a9f821b5a7224a087abcefaf
John Kim <abcdefghijohnk@gmail.com>
2021-01-13 16:48:23 -0700

    Update spec to function with new routes
    Co-Authored-By: Chris Allbritton <cla.inbox@gmail.com>

---
+++ spec/features/services/utilities_index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'As a User' do
+  describe 'When I have clicked a service category' do
+    it 'should see all the businesses for that category' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      json_response = File.read('spec/fixtures/electric_search.json')
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/yelp/80211/utilities/electricity")
+        .to_return(status: 200, body: json_response, headers: {})
+      visit '/80211/utilities'
+
+      click_on 'Electricity'
+
+      expect(current_path).to eq('/80211/utilities/electricity')
+      within(first('.business')) do
+        expect(page).to have_content('Xcel Energy')
+        expect(page).to have_content(1.02)
+      end
+    end
+
+    it 'can click another category for isp' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      json_response = File.read('spec/fixtures/isps.json')
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/yelp/80211/utilities/isps")
+        .to_return(status: 200, body: json_response, headers: {})
+      visit '/80211/utilities'
+
+      click_on 'Internet Service Providers'
+
+      expect(current_path).to eq('/80211/utilities/isps')
+      within(first('.business')) do
+        expect(page).to have_content("Zachs Dope Fiber")
+        expect(page).to have_content(4)
+      end
+    end
+  end
+end

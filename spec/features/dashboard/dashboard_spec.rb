0d3139367878e0402d8913afb626b92e7ccb2f65
Chris Allbritton <cla.inbox@gmail.com>
2021-01-13 16:20:15 -0500

    Remove expectation that utilities is within CSS button
    Co-Authored-By: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-Authored-By: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -24,13 +24,12 @@ describe 'As a user' do
       end
 
       expect(current_path).to eq('/address')
-
-      within '.buttons' do
-        expect(page).to have_button('Utilities')
+        #
+        # expect(page).to have_button('Utilities')
         click_on 'Utilities'
-      end
 
-      expect(current_path).to eq('/80211/Utilities')
+
+      expect(current_path).to eq('/80211/utilities')
       expect(page).to have_content('Utilities')
       expect(page).to have_link('Electricity')
     end
@@ -50,7 +49,7 @@ describe 'As a user' do
       to_return(status: 200, body: json_response, headers: {})
 
        visit '/dashboard'
-       
+
        expect(current_path).to eq("/address")
     end
   end

a4afe2517db0dda0eac30a23ed5d23d4b9195b8f
oxalisviolacea <hannadavis09@gmail.com>
2021-01-13 18:30:56 -0700

    Change file name
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- spec/features/privacy/index_spec.rb
+++ spec/features/privacy/index_spec.rb
@@ -5,7 +5,7 @@ describe 'As a visitor' do
     it 'I should see an the privacy policy' do
       visit privacy_path
       
-      expect(page).to have_content("Relocate's Privacy Notice:")
+      expect(page).to have_content("Relocate's Privacy Policy")
       expect(page).to have_content('When you sign in with your google account, we access your public google user information, but we will only store your google user id, name, and email in our database.')
       expect(page).to have_content('Your google contacts will not be stored or shared with any other entity.')
       expect(page).to have_content('Your information will be used to created a user profile for you on Relocate.')

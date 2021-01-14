09040058845d449d258e9355e72d482accc1a28c
Chris Allbritton <cla.inbox@gmail.com>
2021-01-14 17:08:11 -0500

    Refactor out white space
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- app/services/category_service.rb
+++ app/services/category_service.rb
@@ -2,6 +2,8 @@ class CategoryService
   def self.all_businesses(location, category, type)
     response = conn.get("/api/v1/yelp/#{location}/#{category}/#{type}")
     JSON.parse(response.body, symbolize_names: true)
+
+    
   end
 
   def self.a_business(id)
@@ -14,4 +16,3 @@ class CategoryService
     Faraday.new(url: 'https://relocate-back-end-rails.herokuapp.com')
   end
 end
-

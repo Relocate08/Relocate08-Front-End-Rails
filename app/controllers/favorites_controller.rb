760d786ae3a1045475e921e59df54361d7cf43f8
Zach Stearns <zach.stearns92@gmail.com>
2021-01-13 17:45:34 -0700

    Add create method
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,7 @@
+class FavoritesController < ApplicationController
+  def create
+    FavoritesFacade.save_favorite(current_user.id, params[:yelp_business_id], params[:business_name])
+    flash[:success] = 'Business Saved!'
+    redirect_to "/businesses/#{params[:yelp_business_id]}"
+  end
+end

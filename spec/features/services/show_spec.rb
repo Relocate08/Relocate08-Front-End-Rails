9ee42002b8e2f8f880b4cb88858834a113c80d05
Zach Stearns <zach.stearns92@gmail.com>
2021-01-13 17:58:44 -0700

    WIP, add favorite a business and see it test
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/features/services/show_spec.rb
+++ spec/features/services/show_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe 'As a logged in user' do
   describe 'When I click on a company from the service type index page' do
+    before(:each) do
+      @user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+    
     it 'I am taken to that companys show page to see their info' do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       json_response = File.read('spec/fixtures/show_business_page.json')
       stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/yelp/businesses/9s8dfs7fsdmsdf').
-
+  
         to_return(status: 200, body: json_response, headers: {})
 
       visit '/businesses/9s8dfs7fsdmsdf'
@@ -21,6 +23,54 @@ describe 'As a logged in user' do
         expect(page).to have_content('(800) 895-4999')
         expect(page).to have_content(1.02)
       end
+
+      expect(page).to have_link('Favorite this Business')
+    end
+
+    it 'I can save that business and see saved businesses' do
+      json_response = File.read('spec/fixtures/show_business_page.json')
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/yelp/businesses/9s8dfs7fsdmsdf").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v1.3.0'
+        }).
+      to_return(status: 200, body: json_response, headers: {})
+
+      visit '/businesses/9s8dfs7fsdmsdf'
+
+      json_response = File.read('spec/fixtures/favorite_business.json')
+      stub_request(:post, "https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/#{@user.id}/9s8dfs7fsdmsdf?business_name=Xcel%20Energy").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Length'=>'0',
+        'User-Agent'=>'Faraday v1.3.0'
+        }).
+      to_return(status: 200, body: json_response, headers: {})
+
+
+      click_link('Favorite this Business')
+      
+      expect(current_path).to eq('/businesses/9s8dfs7fsdmsdf')
+      expect(page).to have_content('Business Saved!')
+
+
+      visit '/dashboard'
+
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/#{@user.id}").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Length'=>'0',
+        'User-Agent'=>'Faraday v1.3.0'
+        }).
+      to_return(status: 200, body: json_response, headers: {})
+
+      expect(page).to have_content('Xcel Energy')
     end
   end
 end

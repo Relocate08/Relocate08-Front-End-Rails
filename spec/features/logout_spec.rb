0e2049a88a15cbc61b20fbe623116ae95e65e644
Zach Stearns <zach.stearns92@gmail.com>
2021-01-14 15:30:31 -0700

    Add logout test

--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 describe 'As a logged in user' do
-  xit 'I can log out' do
+  it 'I can log out' do
     stub_omniauth
-    @user = create(:omniauth_mock_user, id: 4)
+    @user = create(:omniauth_mock_user, id: 1)
+    
+    first_login = File.read('spec/fixtures/location_search_null.json')
+    stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/1").
+    to_return(status: 200, body: first_login, headers: {})
+    
+    no_favs = File.read('spec/fixtures/empty_favs.json')
+    stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1").
+    to_return(status: 200, body: no_favs, headers: {})
+    
     visit root_path
 
-    first_login = File.read('spec/fixtures/location_search_null.json')
-    stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/4").
-         to_return(status: 200, body: first_login, headers: {})
-
     click_link 'Login with Google'
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Logout #{@user.name}")
 
-
-    save_and_open_page
+    click_link("Logout #{@user.name}")
+    expect(current_path).to eq(root_path)
+    expect(page).to have_link('Login with Google')
   end
 end

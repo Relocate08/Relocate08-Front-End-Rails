8b164f2e33c069489de092a97fadaf7fa5accfda
Zach Stearns <zach.stearns92@gmail.com>
2021-01-13 23:02:30 -0700

    Add show and destroy actions
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,25 @@
 
 class FavoritesController < ApplicationController
+  def show
+    business_info = FavoritesFacade.show_favorite(params[:business_id])
+    @id = business_info.id
+    @name = business_info.name
+    @image = business_info.image
+    @url = business_info.url
+    @location = business_info.location
+    @phone = business_info.phone
+    @distance = business_info.distance.to_f
+  end
+
   def create
     FavoritesFacade.save_favorite(current_user.id, params[:yelp_business_id], params[:business_name])
     flash[:success] = 'Business Saved!'
-    redirect_to "/businesses/#{params[:yelp_business_id]}"
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    FavoritesFacade.delete_favorite(current_user.id, params[:business_id])
+    flash[:success] = 'Favorite Deleted!'
+    redirect_to dashboard_path
   end
 end

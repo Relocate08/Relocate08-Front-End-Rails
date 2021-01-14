6851f508f5a4ade89ab2a91472a27318a0512c60
Zach Stearns <zach.stearns92@gmail.com>
2021-01-14 15:31:12 -0700

    Lint

--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -4,15 +4,15 @@ describe 'As a logged in user' do
   it 'I can log out' do
     stub_omniauth
     @user = create(:omniauth_mock_user, id: 1)
-    
+
     first_login = File.read('spec/fixtures/location_search_null.json')
-    stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/1").
-    to_return(status: 200, body: first_login, headers: {})
-    
+    stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/location/1')
+      .to_return(status: 200, body: first_login, headers: {})
+
     no_favs = File.read('spec/fixtures/empty_favs.json')
-    stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1").
-    to_return(status: 200, body: no_favs, headers: {})
-    
+    stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+      .to_return(status: 200, body: no_favs, headers: {})
+
     visit root_path
 
     click_link 'Login with Google'
@@ -24,3 +24,10 @@ describe 'As a logged in user' do
     expect(page).to have_link('Login with Google')
   end
 end
+
+Tip: Based on detected gems, the following RuboCop extension libraries might be helpful:
+  * rubocop-rspec (http://github.com/rubocop-hq/rubocop-rspec)
+
+You can opt out of this message by adding the following to your config (see https://docs.rubocop.org/rubocop/extensions.html#extension-suggestions for more options):
+  AllCops:
+    SuggestExtensions: false

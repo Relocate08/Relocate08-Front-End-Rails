be5b2be6256c4d3a0d3c3d11402b9b3e74056876
Zach Stearns <zach.stearns92@gmail.com>
2021-01-13 23:04:43 -0700

    Move favorite create route, add favorite get and delete routes
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/dashboard', to: 'dashboard#index', as: :dashboard
 
+  post '/:user_id', to: 'favorites#create', as: :favorite_create
+  get '/:user_id/favorites/:business_id', to: 'favorites#show'
+  delete '/:user_id/favorites/:business_id', to: 'favorites#destroy'
+
   get '/address', to: 'service_category#index'
   get '/businesses/:id', to: 'service_types#show'
   get '/:location/utilities', to: 'services#utilities_index'
   get '/:location/recreation', to: 'services#recreation_index'
   get '/:location/homeservices', to: 'services#homeservices_index'
   get '/:location/:services/:type', to: 'service_types#index'
-
-  post '/:user_id/favorites', to: 'favorites#create', as: :favorite_create
 
   get '/team', to: 'team#index', as: :team
   get '/privacy', to: 'privacy#index', as: :privacy

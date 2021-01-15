52510adb117bc5f70e8f3dcb55797611625ce15f
Jake Heft <jakeheft@gmail.com>
2021-01-14 19:11:04 -0700

    Add test for event readable time
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>

--- spec/poros/event_spec.rb
+++ spec/poros/event_spec.rb
@@ -51,5 +51,24 @@ describe Event do
 		expect(event.readable_date).to eq('2021-01-15')
 	end
 
-	
+	it '#readable_time' do
+		attr = {id: 'denver-upon-closer-reflection-opening-reception-and-exhibition',
+			type: 'events', 
+			attributes: {
+      attending_count: 69,
+      id: 'denver-upon-closer-reflection-opening-reception-and-exhibition',
+      interested_count: 65,
+      is_canceled: false,
+      is_free: true,
+      location: ['300 W 11th Ave Ste A Denver, CO 80204'],
+      name: 'UPON CLOSER REFLECTION Opening Reception and Exhibition',
+      time_start: '2021-01-15T17:00:00-07:00',
+      url: 'https://www.yelp.com/events/denver-upon-closer-reflection-opening-reception-and-exhibition?adjust_creative=hLVXR0MIhyJhliv5ar9z7g&utm_campaign=yelp_api_v3&utm_medium=api_v3_event_search&utm_source=hLVXR0MIhyJhliv5ar9z7g'
+		}
+	}
+
+		event = Event.new(attr)
+
+		expect(event.readable_time).to eq('5:00pm')
+	end
 end

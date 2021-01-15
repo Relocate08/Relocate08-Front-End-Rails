759c4df68f1b176b9e5273bebd52caf77b2c7c67
Zach Stearns <zach.stearns92@gmail.com>
2021-01-14 18:12:47 -0700

    Add tests for see or not see favorites

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -82,5 +82,41 @@ describe 'As a user' do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content('Please enter a valid zipcode')
     end
+
+    it 'If I have favorites saved, I see them' do
+      stub_omniauth
+      @user = create(:omniauth_mock_user, id: 1)
+  
+      first_login = File.read('spec/fixtures/location_search_null.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/location/1')
+        .to_return(status: 200, body: first_login, headers: {})
+  
+      have_favs = File.read('spec/fixtures/get_favorite.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: have_favs, headers: {})
+  
+      visit root_path
+      click_link 'Login with Google'
+
+      expect(page).to have_link('Xcel Energy')
+    end
+
+    it 'If I do not have favorites saved, I do not see any' do
+      stub_omniauth
+      @user = create(:omniauth_mock_user, id: 1)
+  
+      first_login = File.read('spec/fixtures/location_search_null.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/location/1')
+        .to_return(status: 200, body: first_login, headers: {})
+  
+      no_favs = File.read('spec/fixtures/empty_favs.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: no_favs, headers: {})
+  
+      visit root_path
+      click_link 'Login with Google'
+
+      expect(page).to have_content('You have no favorites.')
+    end
   end
 end

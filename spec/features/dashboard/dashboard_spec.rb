2bdea8b12590145447e88370a1d9c696280678bf
Kevin Cuadros <kev040798@gmail.com>
2021-01-14 21:41:59 -0500

    Test for dashboard created with edge cases.
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Zach Stearns <64452036+Stearnzy@users.noreply.github.com>

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -2,88 +2,78 @@ require 'rails_helper'
 
 describe 'As a user' do
   describe 'when I visit the dashboard page' do
-    xit 'should see a form to fill in a location of search' do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    it 'should see a form to fill in a location of search' do
+      stub_omniauth
+      @user = create(:omniauth_mock_user, id: 1)
+      first_login = File.read('spec/fixtures/location_search_null.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/location/1')
+        .to_return(status: 200, body: first_login, headers: {})
+      no_favs = File.read('spec/fixtures/empty_favs.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: no_favs, headers: {})
 
-      json_response = File.read('spec/fixtures/location_search_null.json')
-      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'Faraday v1.3.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
+      visit root_path
 
-      visit '/dashboard'
+      click_link 'Login with Google'
 
-      
+      expect(current_path).to eq(dashboard_path)
+
       json_response = File.read('spec/fixtures/location_search.json')
-      stub_request(:post, "https://relocate-back-end-rails.herokuapp.com/api/v1/80211/#{user.id}").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'Content-Length'=>'0',
-       	  'User-Agent'=>'Faraday v1.3.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
-         
+      stub_request(:post, "https://relocate-back-end-rails.herokuapp.com/api/v1/80211/#{@user.id}")
+        .to_return(status: 200, body: json_response, headers: {})
+
       within '.location-search' do
         fill_in :location, with: '80211'
         click_on 'Save Location'
       end
 
       expect(current_path).to eq(dashboard_path)
+    end
 
+    it 'should redirect them if the user already has a saved location' do
+      stub_omniauth
+      user = create(:omniauth_mock_user, id: 1)
+      json_response = File.read('spec/fixtures/location_search.json')
+
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}")
+        .to_return(status: 200, body: json_response, headers: {})
+
+      no_favs = File.read('spec/fixtures/empty_favs.json')
+
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: no_favs, headers: {})
+
+      visit root_path
+
+      click_link 'Login with Google'
+
+      visit '/dashboard'
 
       within '.location-search' do
         click_button 'Search With Saved Zipcode'
       end
 
-      expect(current_path).to eq(address_path)
+      expect(current_path).to eq('/address.80110')
       click_on 'Utilities'
 
-      expect(current_path).to eq('/80211/utilities')
+      expect(current_path).to eq('/80110/utilities')
       expect(page).to have_content('Utilities')
       expect(page).to have_link('Electricity')
     end
 
-    xit "should redirect them if the user already has a saved location" do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      json_response = File.read('spec/fixtures/location_search.json')
+    it 'not search with an invalid zipcode' do
+      stub_omniauth
+      @user = create(:omniauth_mock_user, id: 1)
+      first_login = File.read('spec/fixtures/location_search_null.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/location/1')
+        .to_return(status: 200, body: first_login, headers: {})
+      no_favs = File.read('spec/fixtures/empty_favs.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: no_favs, headers: {})
 
-      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}").
-      with(
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent'=>'Faraday v1.3.0'
-        }).
-      to_return(status: 200, body: json_response, headers: {})
+      visit root_path
 
-       visit '/dashboard'
-
-       expect(current_path).to eq(dashboard_path)
-    end
-
-    xit 'not search with an invalid zipcode' do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      json_response = File.read('spec/fixtures/location_search_null.json')
-      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'Faraday v1.3.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
-
-      visit '/dashboard'
+      click_link 'Login with Google'
 
       within '.location-search' do
         fill_in :location, with: '3012q'
@@ -102,21 +92,21 @@ describe 'As a user' do
       expect(page).to have_content('Please enter a valid zipcode')
     end
 
-    xit 'should see button appear if location exists' do 
-      user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    it 'should see button appear if location exists' do
+      stub_omniauth
+      user = create(:omniauth_mock_user, id: 1)
 
       json_response = File.read('spec/fixtures/location_search.json')
-      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'User-Agent'=>'Faraday v1.3.0'
-           }).
-         to_return(status: 200, body: json_response, headers: {})
+      stub_request(:get, "https://relocate-back-end-rails.herokuapp.com/api/v1/location/#{user.id}")
+        .to_return(status: 200, body: json_response, headers: {})
 
-      visit '/dashboard'
+      no_favs = File.read('spec/fixtures/empty_favs.json')
+      stub_request(:get, 'https://relocate-back-end-rails.herokuapp.com/api/v1/favorites/1')
+        .to_return(status: 200, body: no_favs, headers: {})
+
+      visit root_path
+
+      click_link 'Login with Google'
 
       expect(page).to have_button('Search With Saved Zipcode')
     end
